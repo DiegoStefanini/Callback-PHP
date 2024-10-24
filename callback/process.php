1b976a8eaabd35588c59080ffe17a3b01819d373
Diego Stefanini <116094549+DiegoStefanini@users.noreply.github.com>
2024-10-24 15:51:21 +0200

    Add files via upload

---
+++ callback/process.php
@@ -0,0 +1,181 @@
+<?php
+$words = array(
+    "abaco", "abbattimento", "abbandono", "abbondanza", "abitudine", "abito", "abolizione", "aborto", "abbraccio", "abbreviazione",
+    "abuso", "accettazione", "accidente", "accoglienza", "acqua", "acquisto", "accumulare", "acido", "azione", "adattamento",
+    "adempimento", "adrenalina", "adulto", "affetto", "aggressione", "agricoltura", "aiuto", "albergo", "alimento", "amico",
+    "amore", "analisi", "animale", "annuncio", "ansia", "apertura", "appartamento", "applicazione", "approccio", "argomento",
+    "armonia", "arte", "asilo", "assistenza", "astronomia", "atmosfera", "attività", "attore", "attrazione", "avventura",
+    "avviso", "bambino", "banchetto", "barca", "base", "bellezza", "biblioteca", "biscotto", "biologia", "caffè",
+    "calore", "calcio", "campagna", "campione", "canzone", "carattere", "carriera", "cartella", "caso", "cattivo",
+    "cerchio", "cerca", "città", "classe", "cognome", "comunicazione", "compito", "concetto", "confessione", "conoscenza",
+    "consiglio", "contatto", "contratto", "conversazione", "coraggio", "corso", "creazione", "cucina", "curiosità", "decisione",
+    "definizione", "delicato", "denominazione", "desiderio", "design", "determinazione", "diario", "difficoltà", "diritto", "dobbiamo",
+    "donna", "dovere", "dubbio", "eleganza", "emozione", "energia", "entusiasmo", "epoca", "esperienza", "essenza",
+    "età", "famiglia", "fantasia", "felicità", "film", "finestra", "formazione", "forza", "frequenza", "futuro",
+    "gioco", "gioventù", "grazia", "guida", "idea", "immagine", "impatto", "impegno", "importanza", "incontro",
+    "indipendenza", "informazione", "invenzione", "ironia", "istruzione", "lavoro", "leggenda", "libertà", "lingua", "luce",
+    "magia", "malattia", "mamma", "mappa", "marcia", "materia", "medicina", "memoria", "metodo", "messaggio",
+    "modifica", "movimento", "musica", "natura", "negazione", "nella", "notizia", "nuvola", "obiettivo", "occasione",
+    "offerta", "oggetto", "ombrello", "opportunità", "ordine", "organizzazione", "oro", "paesaggio", "passione", "pazienza",
+    "persona", "petizione", "pianeta", "pietra", "pioggia", "poca", "polvere", "possibilità", "preparazione", "presenza",
+    "progetto", "progresso", "proprietà", "pubblicità", "quadro", "ragione", "rappresentazione", "recensione", "relazione", "remoto",
+    "risposta", "risultato", "ritmo", "salute", "sangue", "scolaro", "senso", "servizio", "sogno", "sonno",
+    "sorgente", "spazio", "specialità", "spettacolo", "spirito", "storia", "strada", "studio", "successo", "suggerimento",
+    "svelare", "svolta", "tendenza", "teatro", "tempo", "testo", "tradizione", "trasformazione", "triangolo", "tutela",
+    "umorismo", "unicità", "valore", "varietà", "verità", "viaggio", "vittoria", "vivaio", "voce", "voglia",
+    "voto", "zucchero", "zoo", "abbagliare", "abbondante", "abbellire", "abbandonare", "accelerare", "accettare", "accogliere",
+    "accompagnare", "acquistare", "adeguare", "adottare", "affermare", "agire", "alleviare", "analizzare", "annunciare", "applaudire",
+    "approvare", "arredare", "assumere", "assistere", "attendere", "attraversare", "avanzare", "avvertire", "battere", "baciare",
+    "battere", "brindare", "cambiare", "cercare", "chiudere", "chiedere", "cittadino", "coccolare", "collegare", "comprendere",
+    "concludere", "confermare", "conservare", "consolidare", "costruire", "crescere", "decidere", "dimostrare", "dipingere", "diventare",
+    "dormire", "emergere", "eseguire", "esplorare", "esprimere", "fantasticare", "festeggiare", "giocare", "imparare", "invitare",
+    "lavorare", "mettere", "mischiare", "modificare", "muovere", "offrire", "parlare", "perdere", "prendere", "proporre",
+    "raccogliere", "raggiungere", "recuperare", "riconoscere", "risolvere", "scommettere", "selezionare", "sforzarsi", "sostenere", "studiare",
+    "sperimentare", "trovare", "utilizzare", "valutare", "verificare", "vincere", "visita", "volare", "abbondante", "abbreviare",
+    "abbandonare", "abitudine", "accettare", "accompagnare", "acquisto", "affermare", "affetto", "aggressione", "aiutare", "alleviare",
+    "alimento", "amico", "amore", "analizzare", "animale", "annunciare", "applaudire", "apprezzare", "approvare", "arredare",
+    "articolo", "assistere", "attività", "attraversare", "avanzare", "avvicinare", "bambino", "banchetto", "benedire", "bene",
+    "bello", "biblioteca", "biologia", "biscotto", "calore", "cambiare", "campione", "carriera", "carta", "casa",
+    "cattivo", "cerca", "chiave", "città", "classe", "cognome", "comunicare", "compito", "confermare", "conoscenza",
+    "conservare", "costruire", "crescere", "decidere", "definizione", "delicato", "denominazione", "desiderio", "design", "determinazione",
+    "dialogo", "direzione", "difficoltà", "diploma", "donna", "dovere", "dubbio", "eleganza", "emozione", "energia",
+    "entusiasmo", "esperienza", "essenza", "famiglia", "fantasia", "felicità", "film", "finestra", "formazione", "forza",
+    "frequenza", "futuro", "giocattolo", "gioco", "gioventù", "grazia", "guida", "idea", "immagine", "impatto",
+    "impegno", "importanza", "incontro", "indipendenza", "informazione", "invenzione", "ironia", "istruzione", "lavoro", "legame",
+    "libertà", "lingua", "luce", "magia", "malattia", "mappa", "marcia", "materia", "medicina", "memoria",
+    "metodo", "messaggio", "modifica", "movimento", "musica", "natura", "negazione", "notizia", "obiettivo", "occasione",
+    "offerta", "oggetto", "ombrello", "opportunità", "ordine", "organizzazione", "paesaggio", "passione", "pazienza", "persona",
+    "petizione", "pianeta", "pietra", "pioggia", "poca", "polvere", "possibilità", "preparazione", "presenza", "progetto",
+    "progresso", "proprietà", "pubblicità", "quadro", "ragione", "recensione", "relazione", "risposta", "risultato", "salute",
+    "sangue", "scolaro", "senso", "servizio", "sogno", "sonno", "sorgente", "spazio", "specialità", "spettacolo",
+    "spirito", "storia", "strada", "studio", "successo", "suggerimento", "svelare", "tendenza", "teatro", "tempo",
+    "testo", "tradizione", "trasformazione", "triangolo", "tutela", "umorismo", "unicità", "valore", "varietà", "verità",
+    "viaggio", "vittoria", "voce", "voglia", "voto", "zucchero", "zoo", "abbinare", "abbondanza", "abito",
+    "abrogare", "accadere", "accedere", "accettare", "accompagnare", "acquistare", "adottare", "affermare", "affetto", "agire",
+    "agricoltura", "alleviare", "amare", "analizzare", "annunciare", "applaudire", "apprezzare", "approvare", "arredare", "asilo",
+    "attendere", "attraversare", "avanzare", "bambino", "banchetto", "barca", "base", "bellezza", "biblioteca", "biologia",
+    "calore", "cambiare", "canto", "cappello", "carattere", "casa", "cattivo", "cittadino", "classe", "comunicazione",
+    "compito", "concetto", "confessione", "conoscenza", "consiglio", "contatto", "contratto", "conversazione", "coraggio", "corso",
+    "creazione", "cucina", "curiosità", "decidere", "definizione", "delicato", "desiderio", "design", "determinazione", "diario",
+    "difficoltà", "donna", "dovere", "dubbio", "educazione", "eleganza", "emozione", "energia", "entusiasmo", "esperienza",
+    "essenza", "famiglia", "fantasia", "felicità", "finestra", "formazione", "forza", "frequenza", "futuro", "gioco",
+    "gioventù", "grazia", "guida", "idea", "immagine", "impatto", "impegno", "importanza", "incontro", "indipendenza",
+    "informazione", "invenzione", "ironia", "istruzione", "lavoro", "libertà", "lingua", "luce", "magia", "malattia",
+    "mamma", "mappa", "marcia", "materia", "medicina", "memoria", "metodo", "messaggio", "modifica", "movimento",
+    "musica", "natura", "negazione", "notizia", "obiettivo", "occasione", "offerta", "oggetto", "ombrello", "opportunità",
+    "ordine", "organizzazione", "oro", "paesaggio", "passione", "pazienza", "persona", "petizione", "pianeta", "pietra",
+    "pioggia", "poca", "polvere", "possibilità", "preparazione", "presenza", "progetto", "progresso", "proprietà", "pubblicità",
+    "quadro", "ragione", "rappresentazione", "recensione", "relazione", "remoto", "risposta", "risultato", "ritmo", "salute",
+    "sangue", "scolaro", "senso", "servizio", "sogno", "sonno", "sorgente", "spazio", "specialità", "spettacolo",
+    "spirito", "storia", "strada", "studio", "successo", "suggerimento", "svelare", "svolta", "tendenza", "teatro",
+    "tempo", "testo", "tradizione", "trasformazione", "triangolo", "tutela", "umorismo", "unicità", "valore", "varietà",
+    "verità", "viaggio", "vittoria", "vivaio", "voce", "voglia", "voto", "zucchero", "zoo", "abbagliare",
+    "abbondante", "abbellire", "abbandonare", "accelerare", "accettare", "accogliere", "accompagnare", "acquistare", "adeguare", "adottare",
+    "affermare", "agire", "alleviare", "analizzare", "annunciare", "applaudire", "approvare", "arredare", "assumere", "assistere",
+    "attendere", "attraversare", "avanzare", "avvertire", "battere", "baciare", "battere", "brindare", "cambiare", "cercare",
+    "chiudere", "chiedere", "cittadino", "coccolare", "collegare", "comprendere", "concludere", "confermare", "conservare", "consolidare",
+    "costruire", "crescere", "decidere", "dimostrare", "dipingere", "diventare", "dormire", "emergere", "eseguire", "esplorare",
+    "esprimere", "fantasticare", "festeggiare", "giocare", "imparare", "invitare", "lavorare", "mettere", "mischiare", "modificare",
+    "muovere", "offrire", "parlare", "perdere", "prendere", "proporre", "raccogliere", "raggiungere", "recuperare", "riconoscere",
+    "risolvere", "scommettere", "selezionare", "sforzarsi", "sostenere", "studiare", "sperimentare", "trovare", "utilizzare", "valutare",
+    "verificare", "vincere", "visita", "volare", "abbondante", "abbreviare", "abbandonare", "abitudine", "accettare", "accompagnare",
+    "acquisto", "affermare", "affetto", "aggressione", "aiutare", "alleviare", "alimento", "amico", "amore", "analizzare",
+    "animale", "annunciare", "applaudire", "apprezzare", "approvare", "arredare", "articolo", "assistere", "attività", "attraversare",
+    "avanzare", "bambino", "banchetto", "benedire", "bene", "bello", "biblioteca", "biologia", "biscotto", "calore",
+    "cambiare", "campione", "carriera", "carta", "casa", "cattivo", "cerca", "chiave", "città", "classe",
+    "comunicare", "compito", "concetto", "confessione", "conoscenza", "consiglio", "contatto", "contratto", "conversazione", "coraggio",
+    "corso", "creazione", "cucina", "curiosità", "decidere", "definizione", "delicato", "denominazione", "desiderio", "design",
+    "determinazione", "dialogo", "direzione", "difficoltà", "diploma", "donna", "dovere", "dubbio", "educazione", "eleganza",
+    "emozione", "energia", "entusiasmo", "esperienza", "essenza", "famiglia", "fantasia", "felicità", "finestra", "formazione",
+    "forza", "frequenza", "futuro", "giocattolo", "gioco", "gioventù", "grazia", "guida", "idea", "immagine",
+    "impatto", "impegno", "importanza", "incontro", "indipendenza", "informazione", "invenzione", "ironia", "istruzione", "lavoro",
+    "libertà", "lingua", "luce", "magia", "malattia", "mamma", "mappa", "marcia", "materia", "medicina",
+    "memoria", "metodo", "messaggio", "modifica", "movimento", "musica", "natura", "negazione", "notizia", "obiettivo",
+    "occasione", "offerta", "oggetto", "ombrello", "opportunità", "ordine", "organizzazione", "paesaggio", "passione", "pazienza",
+    "persona", "petizione", "pianeta", "pietra", "pioggia", "poca", "polvere", "possibilità", "preparazione", "presenza",
+    "progetto", "progresso", "proprietà", "pubblicità", "quadro", "ragione", "recensione", "relazione", "risposta", "risultato",
+    "salute", "sangue", "scolaro", "senso", "servizio", "sogno", "sonno", "sorgente", "spazio", "specialità",
+    "spettacolo", "spirito", "storia", "strada", "studio", "successo", "suggerimento", "svelare", "svolta", "tendenza",
+    "teatro", "tempo", "testo", "tradizione", "trasformazione", "triangolo", "tutela", "umorismo", "unicità", "valore",
+    "varietà", "verità", "viaggio", "vittoria", "vivaio", "voce", "voglia", "voto", "zucchero", "zoo", "abbigliare",
+    "abbondare", "abbondante", "abbellire", "abbandonare", "accelerare", "accettare", "accogliere", "accompagnare", "acquistare", "adeguare",
+    "adottare", "affermare", "agire", "alleviare", "analizzare", "annunciare", "applaudire", "approvare", "arredare", "assumere",
+    "assistere", "attendere", "attraversare", "avanzare", "avvertire", "battere", "baciare", "battere", "brindare", "cambiare",
+    "cercare", "chiudere", "chiedere", "cittadino", "coccolare", "collegare", "comprendere", "concludere", "confermare", "conservare",
+    "consolidare", "costruire", "crescere", "decidere", "dimostrare", "dipingere", "diventare", "dormire", "emergere", "eseguire",
+    "esplorare", "esprimere", "fantasticare", "festeggiare", "giocare", "imparare", "invitare", "lavorare", "mettere", "mischiare",
+    "modificare", "muovere", "offrire", "parlare", "perdere", "prendere", "proporre", "raccogliere", "raggiungere", "recuperare",
+    "riconoscere", "risolvere", "scommettere", "selezionare", "sforzarsi", "sostenere", "studiare", "sperimentare", "trovare", "utilizzare",
+    "valutare", "verificare", "vincere", "visita", "volare", "abbondante", "abbreviare", "abbandonare", "abitudine", "accettare",
+    "accompagnare", "acquisto", "affermare", "affetto", "aggressione", "aiutare", "alleviare", "alimento", "amico", "amore",
+    "analizzare", "animale", "annunciare", "applaudire", "apprezzare", "approvare", "arredare", "articolo", "assistere", "attività",
+    "attraversare", "avanzare", "bambino", "banchetto", "benedire", "bene", "bello", "biblioteca", "biologia", "biscotto",
+    "calore", "cambiare", "campione", "carriera", "carta", "casa", "cattivo", "cerca", "chiave", "città",
+    "classe", "comunicare", "compito", "concetto", "confessione", "conoscenza", "consiglio", "contatto", "contratto", "conversazione",
+    "coraggio", "corso", "creazione", "cucina", "curiosità", "decidere", "definizione", "delicato", "denominazione", "desiderio",
+    "design", "determinazione", "dialogo", "direzione", "difficoltà", "diploma", "donna", "dovere", "dubbio", "educazione",
+    "eleganza", "emozione", "energia", "entusiasmo", "esperienza", "essenza", "famiglia", "fantasia", "felicità", "finestra",
+    "formazione", "forza", "frequenza", "futuro", "giocattolo", "gioco", "gioventù", "grazia", "guida", "idea",
+    "immagine", "impatto", "impegno", "importanza", "incontro", "indipendenza", "informazione", "invenzione", "ironia", "istruzione",
+    "lavoro", "libertà", "lingua", "luce", "magia", "malattia", "mamma", "mappa", "marcia", "materia",
+    "medicina", "memoria", "metodo", "messaggio", "modifica", "movimento", "musica", "natura", "negazione", "notizia",
+    "obiettivo", "occasione", "offerta", "oggetto", "ombrello", "opportunità", "ordine", "organizzazione", "paesaggio", "passione",
+    "pazienza", "persona", "petizione", "pianeta", "pietra", "pioggia", "poca", "polvere", "possibilità", "preparazione",
+    "presenza", "progetto", "progresso", "proprietà", "pubblicità", "quadro", "ragione", "recensione", "relazione", "risposta",
+    "risultato", "salute", "sangue", "scolaro", "senso", "servizio", "sogno", "sonno", "sorgente", "spazio",
+    "specialità", "spettacolo", "spirito", "storia", "strada", "studio", "successo", "suggerimento", "svelare", "tendenza",
+    "teatro", "tempo", "testo", "tradizione", "trasformazione", "triangolo", "tutela", "umorismo", "unicità", "valore",
+    "varietà", "verità", "viaggio", "vittoria", "vivaio", "voce", "voglia", "voto", "zucchero", "zoo"
+);
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    foreach ($_POST as $funzione => $valore) {  // per ogni elemento di post, prendo la chiave e il valore (array associativo)
+        if ($valore != NULL) { // se non è stato inserito input $valore = NULL
+            $words = Filtra($words, $funzione, $valore); 
+        }
+    }
+    echo "Sono stati trovati " . count($words) . " parole <br>"; 
+    print_r($words); 
+} else {
+    echo "Nessun dato inviato.";
+}
+
+function IniziaCon($arr, $carattere) {
+    $r = [];
+    foreach ($arr as $parola) {  // per ogni parola in words
+        if (strpos($parola, $carattere) === 0) {  // controlla se inizia con il paramentro che gli mando 
+            $r[] = $parola; // mette la parola nell'array del risultato
+        }
+    }
+    return $r; 
+}
+
+function FinisceCon($arr, $carattere) {
+    $r = [];
+    foreach ($arr as $parola) {  // per ogni parola in words
+        if (substr($parola, -1) === $carattere) { // controlla se finisce con il parametro che gli mando
+            $r[] = $parola; // mette la parola nell'array del risultato
+        }
+    }
+    return $r; 
+}
+
+function Lung($arr, $lunghezza) {
+    $r = [];
+    foreach ($arr as $parola) {  // per ogni parola in words
+        if (strlen($parola) === $lunghezza) { // controlla se è della lunghezza che gli mando
+            $r[] = $parola; // mette la parola nell'array del risultato
+        }
+    }
+    ;
+    return $r; 
+}
+
+
+function Filtra($arr, $callback, $parametro) {
+    return $callback($arr, $parametro);
+}
+
+?>
